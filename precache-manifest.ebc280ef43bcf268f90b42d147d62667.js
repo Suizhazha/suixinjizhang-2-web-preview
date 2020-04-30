806b0ac0a4f86bd4c0dee6a0093309aa23fd20e5
Suizhazha <573505403@qq.com>
2020-04-30 09:07:50 +0800

    deploy

---
+++ precache-manifest.ebc280ef43bcf268f90b42d147d62667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9ead87f8ecca1a01cb",
+    "url": "/suixinjizhang-2-web-preview/css/app.5ea78a40.css"
+  },
+  {
+    "revision": "3915b086dcfadd2dd6e39c8b253cd225",
+    "url": "/suixinjizhang-2-web-preview/index.html"
+  },
+  {
+    "revision": "bd9ead87f8ecca1a01cb",
+    "url": "/suixinjizhang-2-web-preview/js/app.13450ab0.js"
+  },
+  {
+    "revision": "635d4f0f51dfef7818c4",
+    "url": "/suixinjizhang-2-web-preview/js/chunk-vendors.ad7f9b54.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/suixinjizhang-2-web-preview/manifest.json"
+  },
+  {
+    "revision": "650a037fc868be45a41b2b214026600a",
+    "url": "/suixinjizhang-2-web-preview/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/suixinjizhang-2-web-preview/robots.txt"
+  }
+]);
